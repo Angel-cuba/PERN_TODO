8ea4b64c972c14eb20d63faec085c42291e7b7f2
Mac_Developer <araozangel842@gmail.com>
2022-03-09 12:13:59 +0200

    Input component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import InputTodo from './components/InputTodo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Fron side</h1>
+				<InputTodo />
+			</div>
+		</div>
+	);
 }
 
 export default App;
